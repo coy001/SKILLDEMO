d338f134843c058f6d8f3931ca3bf8d3f54f3461
Elvis Yu <617986265@qq.com>
2022-05-01 19:30:18 -0700

    add

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,4 +1,7 @@
 public class SkillDemo{
 //we can start now!
-
+ public static int add(int n, int m)
+ {
+     return n+m;
+ }
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest{
+
+      @Test
+      public void testAdd()
+      {
+          assertEquals(2,SkillDemo.add(1,2));
+      }
+}
